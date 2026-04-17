9f47b1fc664edad4e89d76af5997c8be9f6ee4a2
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2026-04-17 21:33:05 +0200

    update tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(ggseg)
+library(ggseg3d)
+
 describe("glasser atlas", {
   it("is a ggseg_atlas", {
     expect_s3_class(glasser(), "ggseg_atlas")
@@ -12,22 +16,22 @@ describe("glasser atlas", {
     skip_if_not_installed("ggseg")
     skip_if_not_installed("ggplot2")
     skip_if_not_installed("vdiffr")
-    p <- ggplot2::ggplot() +
-      ggseg::geom_brain(
+    p <- ggplot() +
+      geom_brain(
         atlas = glasser(),
-        mapping = ggplot2::aes(fill = label),
-        position = ggseg::position_brain(hemi ~ view),
+        mapping = aes(fill = label),
+        position = position_brain(hemi ~ view),
         show.legend = FALSE
       ) +
-      ggplot2::scale_fill_manual(values = glasser()$palette, na.value = "grey") +
-      ggplot2::theme_void()
+      scale_fill_manual(values = glasser()$palette, na.value = "grey") +
+      theme_void()
     vdiffr::expect_doppelganger("glasser-2d", p)
   })
 
   it("renders with ggseg3d", {
     skip_if_not_installed("ggseg3d")
     skip_if_not_installed("ggseg.meshes")
-    p <- ggseg3d::ggseg3d(atlas = glasser())
+    p <- ggseg3d(atlas = glasser())
     expect_s3_class(p, c("plotly", "htmlwidget"))
   })
 })
